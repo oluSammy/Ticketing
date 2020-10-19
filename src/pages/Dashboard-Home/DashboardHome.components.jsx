dbd5ef1d0ea866db7652c20f38720c3425308b81
oluSammy <olumorinsammy@gmail.com>
2020-10-18 23:09:27 -0700

    deployed to firebase

--- src/pages/Dashboard-Home/DashboardHome.components.jsx
+++ src/pages/Dashboard-Home/DashboardHome.components.jsx
@@ -23,42 +23,42 @@ const DashboardHome = () => {
                         <div className="ticket-box__title">Due Today</div>
                         <BiCalendarWeek className="ticket-box__icon"/>
                     </div>
-                    <div className="ticket-box__number">12</div>
+                    <div className="ticket-box__number">&#9775;</div>
                 </Link>
                 <Link to="/uncompleted" className="ticket-box">
                     <div className="ticket-box__header">
                         <div className="ticket-box__title">Uncompleted</div>
                         <BiMessageAltError className="ticket-box__icon" style={{fill: '#b71c1c '}} />
                     </div>
-                    <div className="ticket-box__number">12</div>
+                    <div className="ticket-box__number">&#9854;</div>
                 </Link>
                 <Link  to="/overdue" className="ticket-box">
                     <div className="ticket-box__header">
                         <div className="ticket-box__title">Overdue</div>
                         <BiCommentError className="ticket-box__icon" style={{fill: 'red'}} />
                     </div>
-                    <div className="ticket-box__number">12</div>
+                    <div className="ticket-box__number">&#9853;</div>
                 </Link>
                 <Link to="/resolved" className="ticket-box">
                     <div className="ticket-box__header">
                         <div className="ticket-box__title">Resolved</div>
                         <BiCheckDouble className="ticket-box__icon" style={{fill: 'green'}} />
                     </div>
-                    <div className="ticket-box__number">12</div>
+                    <div className="ticket-box__number">&#9769;</div>
                 </Link>
                 <Link to="unassigned" className="ticket-box">
                     <div className="ticket-box__header">
                         <div className="ticket-box__title">Not Assigned</div>
                         <MdAssignmentInd className="ticket-box__icon" style={{fill: 'orangered'}} />
                     </div>
-                    <div className="ticket-box__number">12</div>
+                    <div className="ticket-box__number">&#9830;</div>
                 </Link>
                 <Link to="/completed" className="ticket-box">
                     <div className="ticket-box__header">
                         <div className="ticket-box__title">Completed</div>
                         <BiTask className="ticket-box__icon" style={{fill: 'green'}} />
                     </div>
-                    <div className="ticket-box__number">12</div>
+                    <div className="ticket-box__number">&#9883;</div>
                 </Link>
             </div>
         </div>
